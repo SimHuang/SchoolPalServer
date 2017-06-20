68845439dcb8078ed7acb52ebef1eaca5be9129f
Simon Huang <simhuang92@gmail.com>
2017-06-20 01:15:07 -0400

    enable cors

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json({type: '*/*'}));
 
+//enable CORS
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
 app.get('/', function(req, res) {
     res.send({ping:'pong'});
 });
