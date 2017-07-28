878b1765bd2e3f7bb90a602bbd0faa279812761d
Simon Huang <simhuang92@gmail.com>
2017-07-27 20:26:42 -0400

    post api structure

---
+++ controllers/post.js
@@ -0,0 +1,14 @@
+/**
+ * Route api to create a new post
+ */
+module.exports.newPost = function(req, res, next) {
+    console.log('hit this api');
+    res.send({ok:"ok"});
+}
+
+/**
+ * Route api to get a post
+ */
+module.exports.getPost = function(req, res, next) {
+
+}
