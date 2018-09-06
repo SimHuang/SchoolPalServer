b8b6bb76b37393b2a656dc9b965fd44017b4b3f7
Simon Huang <simhuang92@gmail.com>
2018-09-06 18:04:13 -0400

    get upvote api implementation

--- controllers/postMeta.js
+++ controllers/postMeta.js
@@ -1,72 +1,72 @@
-var Post = require('../models/post');
+const Post = require("../models/post");
 
 /**
  * upvote a post base on post id
  */
 module.exports.upvotePost = function(req, res, next) {
-    var postId = req.params.id;
+	var postId = req.params.id;
 
-    Post.findOneAndUpdate({_id: postId}, {
-        $inc: { "meta.votes": 1 }
+    //TODO:check to see if the user has already upvoted the answer
 
-    }, function(err, record) {
-        if(err) {
-            return next(err);
-        }
-        var voteCount = record.meta.votes;
-
-        res.send({
-            voteCount: voteCount
-        });
-    });
-}
+	Post.findOneAndUpdate({_id: postId}, {
+		$inc: { "meta.votes": 1 }
+	}, function(err, record) {
+		if(err) {
+			return next(err);
+		}
+		var voteCount = record.meta.votes;
+		res.send({ voteCount: voteCount});
+	});
+};
 
 /**
  * get all upvote count base on post id
  */
 module.exports.getPostUpvoteCount = function(req, res, next) {
-    //TODO
-}
+	const postId = req.params.id;
+	Post.findOne({_id: postId}, (err, record)=> {
+		if(err) {
+			return next(err);
+		}
+		res.send({"upvotes": record.meta.votes});
+	});
+};
 
 /**
  * get all answer count base on post
  */
 module.exports.getPostAnswerCount = function(req, res, next) {
     //TODO
-}
+};
 
 /**
  * increase post answer count
  */
 module.exports.increasePostAnswerCount = function(req, res, next) {
-    var postId = req.params.id;
+	var postId = req.params.id;
 
-    Post.findOneAndUpdate({_id: postId}, {
-        $inc: { "meta.answers": 1}
+	Post.findOneAndUpdate({_id: postId}, {
+		$inc: { "meta.answers": 1}
 
-    }, function(err, record) {
-        if(err) {
-            return next(err);
-        }
-
-        var answerCount = record.meta.answers;
-
-        res.send({
-            answerCount: answerCount
-        });
-    });
-}
+	}, function(err, record) {
+		if(err) {
+			return next(err);
+		}
+		var answerCount = record.meta.answers;
+		res.send({answerCount: answerCount});
+	});
+};
 
 /**
  * bookmark a post base on post id and username
  */
 module.exports.bookmarkPost = function(req, res, next) {
 
-}
+};
 
 /**
  * get all bookmarks base on user id
  */
 module.exports.getBookmarksForUser = function(req, res, next) {
 
-}
+};
--- models/post.js
+++ models/post.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const postSchema = mongoose.Schema({
-    question: { type: String, required: true},
-    tags: { type: Array },
-    post: { type: String},
-    date: { type: Date, default: Date.now },
-    author: { type: String },
-    answers: [
-        {
-            user: String,
-            answer: String, 
-            date: { type: Date, default: Date.now }
-        }
-    ],
-    meta: {
-        views: { type: Number, default: 0 },
-        answers: { type: Number, default: 0 },
-        votes: { type: Number, default: 0 }
-    }
+	question: { type: String, required: true},
+	tags: { type: Array },
+	post: { type: String},
+	date: { type: Date, default: Date.now },
+	author: { type: String },
+	answers: [
+		{
+			user: String,
+			answer: String, 
+			date: { type: Date, default: Date.now }
+		}
+	],
+	meta: {
+		views: { type: Number, default: 0 },
+		answers: { type: Number, default: 0 },
+		votes: { type: Number, default: 0 }
+	}
 });
 
-const post = mongoose.model('post', postSchema);
+const post = mongoose.model("post", postSchema);
 
 module.exports = post;
